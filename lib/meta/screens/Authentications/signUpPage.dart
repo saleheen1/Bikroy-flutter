0003c551ccd84a7701332c9d45a4840fb01434c7
saleheen <smsaleheen18@gmail.com>
2021-06-07 13:40:08 +0600

    sign up page dropdown added'

--- lib/meta/screens/Authentications/signUpPage.dart
+++ lib/meta/screens/Authentications/signUpPage.dart
@@ -21,6 +21,8 @@ class _SignUpPageState extends State<SignUpPage>
   AnimationController? _controller;
   Animation? _animation;
 
+  String dropdownValue = 'Select Account Type';
+
   @override
   void initState() {
     focusNode = FocusNode();
@@ -60,113 +62,152 @@ class _SignUpPageState extends State<SignUpPage>
           child: SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.all(26.0),
-              child: Center(
-                child: Container(
-                    child: Column(
-                  children: [
-                    // Padding(
-                    //   padding: const EdgeInsets.only(bottom: 70, top: 50),
-                    //   child: Container(
-                    //     height: 35,
-                    //     decoration: BoxDecoration(
-                    //         image: DecorationImage(
-                    //             image: AssetImage('assets/images/logo.png'),
-                    //             fit: BoxFit.fitHeight)),
-                    //   ),
-                    // ),
+              child: Container(
+                  child: Column(
+                children: [
+                  // Padding(
+                  //   padding: const EdgeInsets.only(bottom: 70, top: 50),
+                  //   child: Container(
+                  //     height: 35,
+                  //     decoration: BoxDecoration(
+                  //         image: DecorationImage(
+                  //             image: AssetImage('assets/images/logo.png'),
+                  //             fit: BoxFit.fitHeight)),
+                  //   ),
+                  // ),
 
-                    //Sign up text
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 50, top: 50),
-                      child: Text("Sign up",
-                          style: TextStyle(
-                              color: ConstantColors().greyPrimary,
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold)),
-                    ),
+                  //Sign up text
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 50, top: 50),
+                    child: Text("Sign up",
+                        style: TextStyle(
+                            color: ConstantColors().greyPrimary,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold)),
+                  ),
 
-                    Column(
-                      children: [
-                        //name organization, eshop name
-                        CustomInput(
-                          hintText: "Name or Organization",
-                          onChanged: (value) {},
-                          onSubmitted: (value) {
-                            focusNode.nextFocus();
-                          },
-                          textInputAction: TextInputAction.next,
-                        ),
-                        //Mobile
-                        CustomInput(
-                          isNumberField: true,
-                          hintText: "Mobile",
-                          onChanged: (value) {},
-                          onSubmitted: (value) {
-                            focusNode.nextFocus();
-                          },
-                          textInputAction: TextInputAction.next,
-                        ),
+                  Column(
+                    children: [
+                      //name organization, eshop name
+                      CustomInput(
+                        hintText: "Name or Organization",
+                        onChanged: (value) {},
+                        onSubmitted: (value) {
+                          focusNode.nextFocus();
+                        },
+                        textInputAction: TextInputAction.next,
+                      ),
+                      //Mobile
+                      CustomInput(
+                        isNumberField: true,
+                        hintText: "Mobile",
+                        onChanged: (value) {},
+                        onSubmitted: (value) {
+                          focusNode.nextFocus();
+                        },
+                        textInputAction: TextInputAction.next,
+                      ),
 
-                        //Email
-                        CustomInput(
-                          hintText: "Email",
-                          onChanged: (value) {},
-                          onSubmitted: (value) {
-                            focusNode.nextFocus();
-                          },
-                          textInputAction: TextInputAction.next,
-                        ),
+                      //Email
+                      CustomInput(
+                        hintText: "Email",
+                        onChanged: (value) {},
+                        onSubmitted: (value) {
+                          focusNode.nextFocus();
+                        },
+                        textInputAction: TextInputAction.next,
+                      ),
 
-                        //password
-                        CustomInput(
-                          hintText: "Password",
-                          onChanged: (value) {},
-                          onSubmitted: (value) {
-                            focusNode.nextFocus();
-                          },
-                          textInputAction: TextInputAction.next,
-                          focusNode: focusNode,
-                        ),
+                      //password
+                      CustomInput(
+                        hintText: "Password",
+                        onChanged: (value) {},
+                        onSubmitted: (value) {
+                          focusNode.nextFocus();
+                        },
+                        textInputAction: TextInputAction.next,
+                        focusNode: focusNode,
+                      ),
 
-                        //Confirm Password
-                        CustomInput(
-                          hintText: "Confirm Password",
-                          onChanged: (value) {},
-                          onSubmitted: (value) {
-                            focusNode.nextFocus();
-                          },
-                          focusNode: focusNode,
-                        ),
-                      ],
+                      //Confirm Password
+                      CustomInput(
+                        hintText: "Confirm Password",
+                        onChanged: (value) {},
+                        onSubmitted: (value) {
+                          focusNode.nextFocus();
+                        },
+                        focusNode: focusNode,
+                      ),
+                    ],
+                  ),
+                  //Account type dropdown
+
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 13),
+                    decoration: BoxDecoration(
+                      color: Color(0xfff2f2f2),
+                      // border: Border.all(color: ConstantColors().greySecondary),
+                      borderRadius: BorderRadius.circular(6),
                     ),
-                    SizedBox(
-                      height: 30,
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton<String>(
+                        isExpanded: true,
+                        value: dropdownValue,
+                        icon: const Icon(Icons.keyboard_arrow_down_rounded,
+                            color: Color(0xff646464)),
+                        iconSize: 26,
+                        elevation: 16,
+                        style: const TextStyle(color: Color(0xff646464)),
+                        // underline: Container(
+                        //     height: 1, color: Color(0xffC6C6C6)),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            dropdownValue = newValue!;
+                          });
+                        },
+                        items: <String>[
+                          'Select Account Type',
+                          'Personal Account',
+                          'Business Account',
+                          'E-shop',
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(
+                              value,
+                            ),
+                          );
+                        }).toList(),
+                      ),
                     ),
-                    CustomButton(
-                      text: "Register",
-                      outlineBtn: false,
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            PageTransition(
-                                child: HomePage(),
-                                type: PageTransitionType.rightToLeft));
-                      },
-                    ),
-                    CustomButton(
-                      text: "login",
-                      outlineBtn: true,
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            PageTransition(
-                                child: LoginPage(),
-                                type: PageTransitionType.leftToRight));
-                      },
-                    ),
-                  ],
-                )),
-              ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  CustomButton(
+                    text: "Register",
+                    outlineBtn: false,
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          PageTransition(
+                              child: HomePage(),
+                              type: PageTransitionType.rightToLeft));
+                    },
+                  ),
+                  CustomButton(
+                    text: "login",
+                    outlineBtn: true,
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          PageTransition(
+                              child: LoginPage(),
+                              type: PageTransitionType.leftToRight));
+                    },
+                  ),
+                ],
+              )),
             ),
           ),
         ),
